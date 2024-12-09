ea2ba20d4177314094f9e1412d357b989a3d65cc
Diego <jesustello192002@gmail.com>
2024-12-08 20:17:20 -0500

    viewBinding

--- app/src/main/java/com/dirac/myapplication/HuggingFace/RetrofitBrainTumor.kt
+++ app/src/main/java/com/dirac/myapplication/HuggingFace/RetrofitBrainTumor.kt
@@ -5,11 +5,11 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitBrainTumor {
     private const val BASE_URL = "https://raulhuarote-ejemplo.hf.space/"
-    val getinstance: HuellaApi by lazy {
+    val getinstance: BrainTumorApi by lazy {
         val retrofit = Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
-        retrofit.create(HuellaApi::class.java)
+        retrofit.create(BrainTumorApi::class.java)
     }
 }
